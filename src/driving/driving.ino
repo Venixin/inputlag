9faf182ecb6e55a7e9cdc127cdd27b499c7d234a
Ansel-Kee <a1.kee.jx@gmail.com>
2025-08-16 00:32:11 +0800

    work im begging you

--- src/driving/driving.ino
+++ src/driving/driving.ino
@@ -1,5 +1,11 @@
-#include "AccelStepper.h"
+#include <HUSKYLENS.h>
+#include <Servo.h>
+#include <Wire.h>
 #include <SoftwareSerial.h>
+#include <AccelStepper.h>
+HUSKYLENS huskylens;
+
+void printResult(HUSKYLENSResult result);
 
 // Define stepper motor connections and motor interface type. 
 // Motor interface type must be set to 1 when using a driver:
@@ -9,6 +15,8 @@
 int x = 10;
 int y = 1;
 int t;
+bool left = false;
+bool turned = false;
 
 // Create a new instance of the AccelStepper class:
 AccelStepper stepper = AccelStepper(motorInterfaceType, stepPin, dirPin);
@@ -22,6 +30,7 @@ float speed = 2000;
 void setup() {
   // Set the maximum speed and acceleration:
   Serial.begin(115200);
+  Wire.begin();
   stepper.setMaxSpeed(speed);
   stepper.setAcceleration(speed);
   stepper.setSpeed(speed);
@@ -30,24 +39,62 @@ void setup() {
   myservo.attach(9);  
   // reset to forward
   myservo.write(100);
-  // delay(2000);
+  t = millis();
+  
+  while (!huskylens.begin(Wire)) {
+    Serial.println("Begin failed!");
+    Serial.println("1.Please recheck the \"Protocol Type\" in HUSKYLENS (General Settings>>Protocol Type>> I2C)");
+    Serial.println("2.Please recheck the connection.");
+    delay(1000);
+  }
+  if (!huskylens.writeAlgorithm(ALGORITHM_COLOR_RECOGNITION)) {
+    Serial.print("Failed to set algorithm");
+  }
+
+}
+
+void turn_left(){
+  left = true;
+  myservo.write(75);
+  stepper.runSpeed();
   t = millis();
 }
 
-
+void turn_right(){
+  myservo.write(125);
+  stepper.runSpeed();
+  t = millis();
+}
 //myservo.write(100) is straight
 void loop() { 
-  if (millis() - t >= 1500 && y <= 4) { //Turns right every 2 seconds for 4 times
-    Serial.println(t);
-    myservo.write(50); //Going clockwise around the square (Turning right)
-    stepper.runSpeed();
-    y = y + 1;
-    t = millis();
+  if (huskylens.request()) {
+    if (huskylens.count(1) > 0){
+        Serial.println("RED");
+        turn_left();
+      }
+      if (huskylens.count(2) > 0){
+        Serial.println("BLUE");
+        turn_right();
+      }
   }
 
-  if (millis() - t >= 900 && y <= 5) { //Turns wheel straight again after 1000ms after turning
+
+  if (millis() - t >= 1800 && turned) {
     myservo.write(100);
     stepper.runSpeed();
+    turned = false;
+  }
+
+  if (millis() - t >= 900) {
+    if (left){
+      myservo.write(125);
+      stepper.runSpeed();
+    }
+    else{
+      myservo.write(75);
+      stepper.runSpeed();
+    }
+    turned = true;
   }
 
   stepper.runSpeed();
--- src/huskylens/huskylens.ino
+++ src/huskylens/huskylens.ino
@@ -1,46 +1,49 @@
 #include <HUSKYLENS.h>
-#include <SoftwareSerial.h>
 #include <Servo.h>
 #include <Wire.h>
-
+#include <SoftwareSerial.h>
 HUSKYLENS huskylens;
 
-SoftwareSerial mySerial(0, 1); // RX, TX
-//HUSKYLENS green line >> Pin 10; blue line >> Pin 11
+void printResult(HUSKYLENSResult result);
 
-#define MAGENTA_ID 1
 
 void setup() {
-    Serial.begin(115200);
-    mySerial.begin(9600);
 
-    while (!huskylens.begin(mySerial))
-    {
-        Serial.println(F("Begin failed!"));
-        Serial.println(F("1.Please recheck the \"Protocol Type\" in HUSKYLENS (General Settings>>Protocol Type>>Serial 9600)"));
-        Serial.println(F("2.Please recheck the connection."));
-        delay(1000);
-    }
-
-    if (!huskylens.writeAlgorithm(ALGORITHM_COLOR_RECOGNITION)) {
-        Serial.print("Failed to set algorithm");
-        while (true);
-    }
-
-    
-
+  Serial.begin(115200);
+  Wire.begin();
+  Serial.println("Begin");
+  while (!huskylens.begin(Wire)) {
+    Serial.println("Begin failed!");
+    Serial.println("1.Please recheck the \"Protocol Type\" in HUSKYLENS (General Settings>>Protocol Type>> I2C)");
+    Serial.println("2.Please recheck the connection.");
+    delay(1000);
+  }
+  Serial.println("next");
+  if (!huskylens.writeAlgorithm(ALGORITHM_COLOR_RECOGNITION)) {
+    Serial.print("Failed to set algorithm");
+  }
+  Serial.println("Started");
 }
 
 int magentaBlocks = 0;
 
 void loop() {
-    if (huskylens.request()) {
+  if (huskylens.request()) {
 
-        for (int i = 0; i < huskylens.count(); i++) {
-            HUSKYLENSResult result = huskylens.get(i);
-            magentaBlocks = huskylens.count(1);
-            Serial.println(magentaBlocks);
-        }
+    for (int i = 0; i < huskylens.count(); i++) {
+      HUSKYLENSResult result = huskylens.get(i);
+      magentaBlocks = huskylens.count(1);
+      if (huskylens.count(1) > 0){
+        Serial.println("RED");
       }
-    delay(100);
-} 
+      if (huskylens.count(2) > 0){
+        Serial.println("BLUE");
+      }
+      if (huskylens.count(3) > 0){
+        Serial.println("GREEN");
+      }
+      Serial.println();
+    }
+  }
+  delay(100);
+}
