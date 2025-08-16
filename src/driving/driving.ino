7511a7829b91f99511b856fc4dc3022963a59cea
Ansel-Kee <a1.kee.jx@gmail.com>
2025-08-16 17:19:14 +0800

    Update driving.ino

--- src/driving/driving.ino
+++ src/driving/driving.ino
@@ -5,8 +5,18 @@
 #include <AccelStepper.h>
 HUSKYLENS huskylens;
 
-void printResult(HUSKYLENSResult result);
-
+void printResult(HUSKYLENSResult result){
+    if (result.command == COMMAND_RETURN_BLOCK){
+   Serial.println(String() + F("Block:xCenter=") + result.xCenter + F(",yCenter=") + result.yCenter + F(",width=") + result.width + F(",height=") + result.height + F(",ID=") + result.ID);
+    }
+}
+void display(int id){
+  int count = huskylens.count(id);
+    for (int i=0; i<count;i++){
+      HUSKYLENSResult result = huskylens.getBlock(id, i);
+      printResult(result);
+    }
+}
 // Define stepper motor connections and motor interface type. 
 // Motor interface type must be set to 1 when using a driver:
 #define dirPin 2
@@ -16,6 +26,7 @@ int x = 10;
 int y = 1;
 int t;
 bool left = false;
+bool right = false;
 bool turned = false;
 
 // Create a new instance of the AccelStepper class:
@@ -57,24 +68,25 @@ void turn_left(){
   left = true;
   myservo.write(75);
   stepper.runSpeed();
-  t = millis();
 }
 
 void turn_right(){
+  right = true;
   myservo.write(125);
   stepper.runSpeed();
-  t = millis();
 }
 //myservo.write(100) is straight
 void loop() { 
   if (huskylens.request()) {
-    if (huskylens.count(1) > 0){
+    if (huskylens.count(1) > 0 && left == false){
         Serial.println("RED");
         turn_left();
+        t = millis();
       }
-      if (huskylens.count(2) > 0){
+      if (huskylens.count(2) > 0 && right == false){
         Serial.println("BLUE");
         turn_right();
+        t = millis();
       }
   }
 
@@ -83,20 +95,34 @@ void loop() {
     myservo.write(100);
     stepper.runSpeed();
     turned = false;
+    left = false;
+    right = false;
+    Serial.println("1");
+    t = millis();
   }
 
-  if (millis() - t >= 900) {
+  if (millis() - t >= 900 && millis()-t < 1800) {
     if (left){
       myservo.write(125);
       stepper.runSpeed();
+      Serial.println("2");
+      turned = true;
     }
-    else{
+    else if (right){
       myservo.write(75);
       stepper.runSpeed();
+      Serial.println("3");
+      turned = true;
     }
-    turned = true;
   }
-
+  if (millis()-t > 200){
+    Serial.println("RED");
+    display(4);
+    Serial.println("GREEN");
+    display(3);
+    Serial.println();
+    t = millis();
+  }
   stepper.runSpeed();
   
 }
