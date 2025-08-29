035022454f0351d47c7cd48e2a8ac68da0761cd3
Ansel-Kee <a1.kee.jx@gmail.com>
2025-08-29 16:07:57 +0800

    Update SQUARE.ino

--- src/SQUARE/SQUARE.ino
+++ src/SQUARE/SQUARE.ino
@@ -1,5 +1,8 @@
 #include "AccelStepper.h"
 #include <SoftwareSerial.h>
+#include <Wire.h>
+#include <HUSKYLENS.h>
+HUSKYLENS huskylens;
 
 // Define stepper motor connections and motor interface type. 
 // Motor interface type must be set to 1 when using a driver:
@@ -10,7 +13,13 @@ int x = 10;
 int y = 1;
 unsigned long t;
 bool left = true;
-
+bool check = true;
+int yellowCount = 0;
+int blueCount = 0;
+int blueID = 2;
+int yellowID = 1;
+bool turning = false;
+int startBtn = 7;
 // Create a new instance of the AccelStepper class:
 AccelStepper stepper = AccelStepper(motorInterfaceType, stepPin, dirPin);
 
@@ -19,10 +28,12 @@ AccelStepper stepper = AccelStepper(motorInterfaceType, stepPin, dirPin);
 Servo myservo;  // create servo object to control a servo
 float speed = 2000;
 
-
+unsigned long n = 0;
 void setup() {
   // Set the maximum speed and acceleration:
   Serial.begin(115200);
+  Wire.begin();
+
   stepper.setMaxSpeed(speed);
   stepper.setAcceleration(speed);
   stepper.setSpeed(speed);
@@ -31,43 +42,60 @@ void setup() {
   myservo.attach(9);  
   // reset to forward
   myservo.write(100);
+  while (!huskylens.begin(Wire)) {
+    Serial.println("Begin failed!");
+    Serial.println("1.Please recheck the \"Protocol Type\" in HUSKYLENS (General Settings>>Protocol Type>> I2C)");
+    Serial.println("2.Please recheck the connection.");
+    delay(1000);
+  }
+  if (!huskylens.writeAlgorithm(ALGORITHM_COLOR_RECOGNITION)) {
+    Serial.print("Failed to set algorithm");
+  }
   // delay(2000);
   t = millis();
+  pinMode(startBtn), INPUT_PULLUP);
+  while(digitalRead(startBtn) == LOW){}
+  Serial.println("start");
 }
 
 
 //myservo.write(100) is straight
+// 150cm in 3500ms
 void loop() { 
-  if (millis() - t >= 10000 && y%2==0) { //Turns right every 2 seconds for 4 times
-    Serial.println(1);
-    if (left){
-      myservo.write(55);
+  
+  if (check && n%5000 == 0){
+    n = 0;
+    if (huskylens.requestBlocks()){
+      Serial.println("checking");
+      int yellowCount = huskylens.count(yellowID);
+      int blueCount = huskylens.count(blueID);
+      if (yellowCount > 0 && blueCount > 0){
+        HUSKYLENSResult yellowResult = huskylens.getBlock(yellowID, 0);
+        HUSKYLENSResult blueResult = huskylens.getBlock(blueID, 0);
+        
+        left = yellowResult.yCenter > blueResult.yCenter;
+        Serial.println(left);
+        check = false;
+      }
     }
-    else{
-      myservo.write(145);
-    }
-    stepper.runSpeed();
-    y = y + 1;
-    Serial.print(millis()-t);
-    t = millis();
   }
-  if (millis() - t >= 7500 && y%2==1) { //Turns right every 2 seconds for 4 times
-    Serial.println(1);
+  n+= 1;
+  if (millis() - t >= 3500 && !turning) { //Turns right every 2 seconds for 4 times
     if (left){
       myservo.write(55);
     }
     else{
       myservo.write(145);
-    }//Going clockwise around the square (Turning right)
+    }
     stepper.runSpeed();
-    y = y + 1;
-    Serial.print(millis()-t);
     t = millis();
+    turning = true;
   }
 
-  if (millis() - t >= 1050) { //Turns wheel straight again after 1000ms after turning
+  if (millis() - t >= 1050 && turning) { //Turns wheel straight again after 1000ms after turning
     myservo.write(100);
     stepper.runSpeed();
+    turning = false;
   }
   // myservo.write(70);
   stepper.runSpeed();
